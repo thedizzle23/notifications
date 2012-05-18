b527d1c3ef9b03ae2b3ce62a75578a382474d347
Robert DeNicola <thedizzle23@gmail.com>
2012-05-18 16:56:49 -0400

    popcorn

---
+++ CoolestApp2/CoolestApp2Tests/CoolestApp2Tests.h
@@ -0,0 +1,13 @@
+//
+//  CoolestApp2Tests.h
+//  CoolestApp2Tests
+//
+//  Created by Robert DeNicola on 5/18/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CoolestApp2Tests : SenTestCase
+
+@end
---
+++ CoolestApp2/CoolestApp2Tests/CoolestApp2Tests.m
@@ -0,0 +1,32 @@
+//
+//  CoolestApp2Tests.m
+//  CoolestApp2Tests
+//
+//  Created by Robert DeNicola on 5/18/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "CoolestApp2Tests.h"
+
+@implementation CoolestApp2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CoolestApp2Tests");
+}
+
+@end
